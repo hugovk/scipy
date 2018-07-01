5c4f497930d4e8a24b74d82fc7db3b11c33042d0
Hugo <hugovk@users.noreply.github.com>
2018-07-01 09:06:08 +0300

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Topic :: Software Development
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
